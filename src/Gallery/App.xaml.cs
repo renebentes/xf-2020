c564d795d9184d7f024bc1e3ccfa54b9cf7b5f15
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-10-10 20:20:17 -0300

    feat: Cria projeto de app Gallery

---
+++ src/Gallery/App.xaml.cs
@@ -0,0 +1,11 @@
+namespace Gallery;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ src/Gallery/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+namespace Gallery;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
