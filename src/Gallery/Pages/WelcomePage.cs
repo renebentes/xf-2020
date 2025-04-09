deaf123840ffebb418a053937ce685cd50efb1ea
Rene B. Pinto <renebentes@yahoo.com.br>
2025-04-09 15:57:40 -0300

    feat: define página de boas vindas do aplicativo

---
+++ src/Gallery/Pages/WelcomePage.cs
@@ -0,0 +1,34 @@
+using CommunityToolkit.Maui.Markup;
+
+namespace Gallery.Pages;
+
+public sealed partial class WelcomePage : Microsoft.Maui.Controls.ContentPage
+{
+    public WelcomePage()
+    {
+        Title = "Bem-vindo";
+        Content = new VerticalStackLayout
+        {
+            Padding = new Thickness(30, 0),
+            Spacing = 25,
+            Children =
+            {
+                new Label { Text = "Bem-vindo ao Gallery!" }
+                    .Font(size: 32)
+                    .TextCenter()
+                    .SemanticHeadingLevel(SemanticHeadingLevel.Level1),
+                new Label
+                {
+                    Text = """
+                        Esta é uma aplicação de exemplo para o .NET MAUI, que visa demonstrar
+                        o uso de diversos layouts, tipos de páginas e controles disponíveis
+                        na plataforma.
+                        """,
+                }
+                    .Font(size: 18)
+                    .TextCenter()
+                    .SemanticHeadingLevel(SemanticHeadingLevel.Level2),
+            },
+        }.CenterVertical();
+    }
+}
