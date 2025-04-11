91bd3fe658f43fa1587ebabc92bff1b6ec46b337
Rene Bentes Pinto <renebentes@yahoo.com.br>
2025-04-10 21:19:04 -0300

    feat(ContentPage): exibe página de conteúdo simples

--- src/Gallery/Pages/PagesGalleryViewModel.cs
+++ src/Gallery/Pages/PagesGalleryViewModel.cs
@@ -1,13 +1,8 @@
 namespace Gallery.Pages;
 
-public sealed partial class PagesGalleryViewModel : BaseGalleryViewModel
-{
-    public PagesGalleryViewModel()
-        : base([
-            Section.Create<NavigationViewModel>(nameof(NavigationPage), string.Empty),
-            Section.Create<FlyoutViewModel>(nameof(FlyoutPage), string.Empty),
-            Section.Create<TabbedViewModel>(nameof(TabbedPage), string.Empty)]
-        )
-    {
-    }
-}
+public sealed partial class PagesGalleryViewModel()
+    : BaseGalleryViewModel([
+            Section.Create<ContentViewModel>(nameof(ContentPage), "Página simples com conteúdo."),
+            Section.Create<NavigationViewModel>(nameof(NavigationPage), "Permite uma navegação hierárquica."),
+            Section.Create<FlyoutViewModel>(nameof(FlyoutPage), "Permite uma navegação mestre-detalhe."),
+            Section.Create<TabbedViewModel>(nameof(TabbedPage), "Permite uma navegação entre abas.")]);
