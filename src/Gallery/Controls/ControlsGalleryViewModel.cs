af45e1dc77577b28e4b6dbd321c08663360c759a
Rene Bentes Pinto <renebentes@yahoo.com.br>
2025-04-10 21:14:14 -0300

    refactor: substitui literal por nameof
    Ao usar
    ameof mantemos o título da seção ligado ao tipo, se
    o nome do tipo for modificado o título acompanhará a mudança.

--- src/Gallery/Controls/ControlsGalleryViewModel.cs
+++ src/Gallery/Controls/ControlsGalleryViewModel.cs
@@ -4,17 +4,20 @@ public sealed partial class ControlsGalleryViewModel : BaseGalleryViewModel
 {
     public ControlsGalleryViewModel()
         : base([
-            Section.Create<BorderViewModel>("Border", "Desenha uma borda, plano de fundo ou ambos."),
-            Section.Create<BoxViewModel>("BoxView", "Desenha um retângulo ou quadrado."),
-            Section.Create<ButtonViewModel>("Button", "Exibe um texto que responde a um comando."),
-            Section.Create<ContentViewModel>("ContentView","Permite criar controles personalizados."),
-            Section.Create<EditorViewModel>("Editor", "Exibe um campo multilinha de texto editável."),
-            Section.Create<EntryViewModel>("Entry", "Exibe um campo de texto editável."),
-            Section.Create<FrameViewModel>("Frame", "Desenha uma borda em torno de um controle."),
-            Section.Create<ImageButtonViewModel>("ImageButton", "Exibe uma imagem que responde a um comando."),
-            Section.Create<ImageViewModel>("Image", "Exibe uma imagem."),
-            Section.Create<LabelViewModel>("Label", "Exibe um texto."),
-            Section.Create<ScrollViewViewModel>("ScrollView", "Permite rolar o conteúdo."),])
+            Section.Create<BorderViewModel>(nameof(Border), "Desenha uma borda, plano de fundo ou ambos."),
+            Section.Create<BoxViewModel>(nameof(BoxView), "Desenha um retângulo ou quadrado."),
+            Section.Create<ButtonViewModel>(nameof(Button), "Exibe um texto que responde a um comando."),
+            Section.Create<ContentViewModel>(nameof(ContentView),"Permite criar controles personalizados."),
+            Section.Create<EditorViewModel>(nameof(Editor), "Exibe um campo multilinha de texto editável."),
+            Section.Create<EntryViewModel>(nameof(Entry), "Exibe um campo de texto editável."),
+#pragma warning disable CS0618 // Type or member is obsolete
+            Section.Create<FrameViewModel>(nameof(Frame), "Desenha uma borda em torno de um controle."),
+#pragma warning restore CS0618 // Type or member is obsolete
+            Section.Create<ImageButtonViewModel>(nameof(ImageButton), "Exibe uma imagem que responde a um comando."),
+            Section.Create<ImageViewModel>(nameof(Image), "Exibe uma imagem."),
+            Section.Create<LabelViewModel>(nameof(Label), "Exibe um texto."),
+            Section.Create<ScrollViewViewModel>(nameof(ScrollView), "Permite rolar o conteúdo."),
+            Section.Create<CheckBoxViewModel>(nameof(CheckBox), "Exibe uma caixa de seleção.")])
     {
     }
 }
